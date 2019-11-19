b6e18121b1d76d1eef84e6414ab0a6626527fa97
Dave <dab414@lehigh.edu>
2019-11-19 15:47:19 -0500

    Patching census scrape

--- Phase1-FindSimilarCompetitionRegions/demographics/census_scrape.py
+++ Phase1-FindSimilarCompetitionRegions/demographics/census_scrape.py
@@ -56,29 +56,33 @@ def webScrape(zipcodes):
 
         ## LEFT OFF HERE
 
-        male = int(re.search(r'r3.*Male.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        female = int(re.search(r'r4.*Female.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        median_age = float(re.search(r'r18.*Median age.*?>(\d.*?)<\/td>', html).group(1))
-        under_five = int(re.search(r'r5.*Under 5 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        ten_fourteen = int(re.search(r'r6.*5 to 9 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r7.*10 to 14 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        fifteen_twentyfour = int(re.search(r'r8.*15 to 19 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r9.*20 to 24 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        twentyfive_fortyfour = int(re.search(r'r10.*25 to 34 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r11.*35 to 44 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        fortyfive_fiftynine = int(re.search(r'r12.*45 to 54 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r13.*55 to 59 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        sixty_seventyfour = int(re.search(r'r14.*60 to 64 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r15.*65 to 74 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        total = int(re.search(r'r30.*Total population.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        white = int(re.search(r'r34.*White.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        black = int(re.search(r'r35.*Black or African American.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        asian = int(re.search(r'r41.*Asian.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        native = int(re.search(r'r49.*Native Hawaiian and Other Pacific Islander.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        two_plus = int(re.search(r'r32.*Two or more races.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        ## throwing some weird error - see note below
-        hispanic = 0
-        hispanic = int(re.search(r'r70.*Hispanic or Latino.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
-        non_hispanic = int(re.search(r'r75.*Not Hispanic or Latino.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+        try:
+          male = int(re.search(r'r3.*Male.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          female = int(re.search(r'r4.*Female.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          median_age = float(re.search(r'r18.*Median age.*?>(\d.*?)<\/td>', html).group(1))
+          under_five = int(re.search(r'r5.*Under 5 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          ten_fourteen = int(re.search(r'r6.*5 to 9 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r7.*10 to 14 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          fifteen_twentyfour = int(re.search(r'r8.*15 to 19 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r9.*20 to 24 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          twentyfive_fortyfour = int(re.search(r'r10.*25 to 34 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r11.*35 to 44 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          fortyfive_fiftynine = int(re.search(r'r12.*45 to 54 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r13.*55 to 59 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          sixty_seventyfour = int(re.search(r'r14.*60 to 64 years.*?>(\d.*?)<\/td>', html).group(1).replace(',','')) + int(re.search(r'r15.*65 to 74 years.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          total = int(re.search(r'r30.*Total population.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          white = int(re.search(r'r34.*White.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          black = int(re.search(r'r35.*Black or African American.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          asian = int(re.search(r'r41.*Asian.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          native = int(re.search(r'r49.*Native Hawaiian and Other Pacific Islander.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          two_plus = int(re.search(r'r32.*Two or more races.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          ## throwing some weird error - see note below
+          hispanic = 0
+          hispanic = int(re.search(r'r70.*Hispanic or Latino.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
+          non_hispanic = int(re.search(r'r75.*Not Hispanic or Latino.*?>(\d.*?)<\/td>', html).group(1).replace(',',''))
 
-      ## complaining that hispanic is referenced before assignment
-        final_data.append([zipcode, male, female, total, white, black, asian, native, two_plus, hispanic, non_hispanic,\
-        median_age, under_five, ten_fourteen, fifteen_twentyfour, twentyfive_fortyfour, fortyfive_fiftynine, sixty_seventyfour])
+        ## complaining that hispanic is referenced before assignment
+          final_data.append([zipcode, male, female, total, white, black, asian, native, two_plus, hispanic, non_hispanic,\
+          median_age, under_five, ten_fourteen, fifteen_twentyfour, twentyfive_fortyfour, fortyfive_fiftynine, sixty_seventyfour])
+
+        except:
+          final_data.append([0]*18)          
       
       ## progress tracker
       if len(zipcodes) > 100:
